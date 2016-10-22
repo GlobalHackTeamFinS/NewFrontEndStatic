7e415545a7aaddace299d3f2e5b75b0889a89814
Aaron Holderman <aaron.holderman@manifest.com>
2016-10-22 16:54:01 -0500

    correct typo

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $(".shelter-toggle" ).click(function() {
   	var parentComponent = $(this).parents('.shelter-list-component');
-  	if (parentComponent.hasClass('open'))0{
+  	if (parentComponent.hasClass('open')){
   		parentComponent.removeClass('open');
   	} else {
   		parentComponent.addClass('open');
