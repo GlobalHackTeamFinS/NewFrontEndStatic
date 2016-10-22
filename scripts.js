8b558249b4662f0e1e03f482d2f5235f75ac6480
Aaron Holderman <aaron.holderman@manifest.com>
2016-10-22 15:05:41 -0500

    align show button, add sample json

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
-
 $(document).ready(function () {
   $(".shelter-toggle" ).click(function() {
-  $( ".more-info" ).slideToggle( "slow");
-  var button = $(this);
-  button.text(button.text() == "Show Less" ? "Show More" : "Show Less");
+	  $( ".more-info" ).slideToggle( "slow");
+	  var button = $(this);
+	  button.text(button.text() == "SHOW LESS" ? "SHOW MORE" : "SHOW LESS");
   });
 });
